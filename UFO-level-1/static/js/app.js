4afe30c1756dc9409469ad6f468eb3f0e15dcfd2
code_red <72528267+Corters22@users.noreply.github.com>
2021-04-07 19:50:18 -0500

    added date to table on html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// adding data into table
+
+// Use D3 to select the table
+var table1 = d3.select("#ufo-table");
+// Use D3 to select the table body
+var tableBody = d3.select("tbody");
+
+function addData (userObject) {
+    userObject.forEach((report)=> {
+        var newRow = tableBody.append('tr');
+        newRow.append('td').text(report.datetime);
+        var city = report['city'].toUpperCase();
+        newRow.append('td').text(city);
+        var state = report['state'].toUpperCase();
+        newRow.append('td').text(state);
+        var country = report['country'].toUpperCase();
+        newRow.append('td').text(country);
+        var shape = report['shape'].toUpperCase();
+        newRow.append('td').text(shape);
+        var duration = report['durationMinutes'].toUpperCase();
+        newRow.append('td').text(duration);
+        newRow.append('td').text(report.comments);
+    })
+};
+
+addData(tableData);

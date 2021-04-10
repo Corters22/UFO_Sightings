80e3a5bfe9f7ae7b17fcd644369a5d90aef78d2b
code_red <72528267+Corters22@users.noreply.github.com>
2021-04-10 18:58:01 -0500

    added clear filter button on level 1 and updated screenshots

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,10 +19,12 @@ data.forEach((report) => {
 
 var button = d3.select("#filter-btn")
 var form = d3.select('form')
+var clearButton = d3.select('#clear-btn')
 
 // Create event handlers for clicking the button or pressing the enter key
 button.on("click", runEnter);
 form.on("submit",runEnter);
+clearButton.on('click', runClear);
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -54,4 +56,24 @@ function runEnter() {
       });
 };
 
+function runClear() {
+  // Clearing values in the inputs (placeholder goes back)
+  inputDate = d3.select("#datetime").property("value", '');
+  inputCity = d3.select("#city").property("value", '');
+  inputState = d3.select("#state").property("value", '');
+  inputCountry = d3.select("#country").property("value", '');
+  inputShape = d3.select("#shape").property("value", '');
+
+  // Clear filtered table
+  tableBody.html('')
+
+  // Adding back original table
+  tableData.forEach((report) => {
+    var row = tableBody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
 

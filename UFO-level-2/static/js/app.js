80e06e54e355762c93e5a13d63c1d2a57c87b767
code_red <72528267+Corters22@users.noreply.github.com>
2021-04-10 18:37:40 -0500

    finished bonus and added images

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -8,7 +8,7 @@ var tableBody = d3.select("tbody");
 
 // adding data into table
 
-data.forEach((report) => {
+tableData.forEach((report) => {
     var row = tableBody.append("tr");
     Object.entries(report).forEach(([key, value]) => {
       var cell = row.append("td");
@@ -17,12 +17,14 @@ data.forEach((report) => {
   });
 
 
-var button = d3.select("#filter-btn")
-var form = d3.selectAll('form')
+var filterButton = d3.select("#filter-btn")
+var clearButton = d3.select('#clear-btn')
+
 
 // Create event handlers for clicking the button or pressing the enter key
-button.on("click", runEnter);
-form.on("submit",runEnter);
+filterButton.on("click", runEnter);
+clearButton.on('click', runClear);
+// form.on("change",runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -49,45 +51,77 @@ function runEnter() {
     console.log("city entered:", cityValue);
     console.log("state entered:", stateValue);
     console.log("country entered:", countryValue);
-    console.log("shape entered:", shapeValue);
+    console.log("shape entered:", shapeValue);  
 
-    filteredObject = {}
-
-    if (dateValue === '') {
-      return tableData;
+    //Running if/else statements to only filter by fields that have a value. If there is no value in the field,
+    // changing the name of the list to add to the table body.
+      
+    if (dateValue != '') {
+      var filteredByDate = tableData.filter(report => report.datetime === dateValue);
     }
     else {
-      filteredByDate = tableData.filter(report => report.datetime === dateValue);
+      filteredByDate = tableData;
     }
-
-    if (cityValue === '' && dateValue === '') {
-      return tableData;
-    }
-    else if (dateValue === '' && cityValue != '') {
-      return filteredByCity = tableData.filter(report => report.city === cityValue);
+    if (cityValue != '') {
+        var filteredByCity = filteredByDate.filter(report => report.city === cityValue);
     }
     else {
-      return filteredByCityAndDate = filteredByDate.filter(report => report.city === cityValue);
+      filteredByCity = filteredByDate;
+    }
+    if (stateValue != '') {
+      var filteredByState = filteredByCity.filter(report => report.state === stateValue);
+    }
+    else {
+      filteredByState = filteredByCity;
+    }
+    if (countryValue != '') {
+      var filteredByCountry = filteredByState.filter(report => report.country === countryValue);
+    }
+    else {
+      filteredByCountry = filteredByState;
+    }
+    if (shapeValue != '') {
+      var filteredData = filteredByCountry.filter(report => report.shape === shapeValue);
+    }
+    else {
+      filteredData = filteredByCountry;
     }
     
-      
-    // var filteredByDate = tableData.filter(report => report.datetime === dateValue);
-    // var filteredByCity = filteredByDate.filter(report => report.city === cityValue);
-    // var filteredByState = filteredByCity.filter(report => report.state === stateValue);
-    // var filteredByCountry = filteredByState.filter(report => report.country === countryValue);
-    // var filteredData = filteredByCountry.filter(report => report.shape === shapeValue);
+    console.log('filteredData:', filteredData);
 
-    // console.log('filteredData:', filteredData);
+    // Clearing table
+    tableBody.html('')
 
-    // tableBody.html('')
-
-    // filteredData.forEach((report) => {
-    //     var row = tableBody.append("tr");
-    //     Object.entries(report).forEach(([key, value]) => {
-    //       var cell = row.append("td");
-    //       cell.text(value);
-    //     });
-    //   });
+    // Adding filtered values to the table
+    filteredData.forEach((report) => {
+        var row = tableBody.append("tr");
+        Object.entries(report).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+  
 };
 
+// function definition for clearing the filters and replacing the table with all data.
 
+function runClear() {
+  // Clearing values in the inputs (placeholder goes back)
+  inputDate = d3.select("#datetime").property("value", '');
+  inputCity = d3.select("#city").property("value", '');
+  inputState = d3.select("#state").property("value", '');
+  inputCountry = d3.select("#country").property("value", '');
+  inputShape = d3.select("#shape").property("value", '');
+
+  // Clear filtered table
+  tableBody.html('')
+
+  // Adding back original table
+  tableData.forEach((report) => {
+    var row = tableBody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
